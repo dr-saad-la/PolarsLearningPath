c63383c31bb238dc5b78d7aa0b4a33d23ae7e823
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-08-24 23:52:44 +0100

    Update download_datasets script

--- scripts/download_datasets.py
+++ scripts/download_datasets.py
@@ -1,18 +1,14 @@
 """
 This module provides a utility function to download and extract zipped CSV datasets from a specified URL.
 
-The main function, `download_zipped_csv_data`, handles downloading a zip file from a given URL, extracting its 
-contents, and saving them to a specified directory. This is particularly useful for preparing datasets for 
-data analysis or machine learning tasks.
+The main function, `download_zipped_csv_data`, handles downloading a zip file from a given URL, extracting its contents, and saving them to a specified directory. This is particularly useful for preparing datasets for data analysis or machine learning tasks.  # noqa: E501
 
 
 Author:
 - Dr. Saad Laouadi
 
 License:
-- This material is intended for educational purposes only and may not be used directly in courses, 
-  video recordings, or similar without prior consent from the author. Proper credit must be attributed 
-  to the author when using or referencing this material.
+- This material is intended for educational purposes only and may not be used directly in courses, video recordings, or similar without prior consent from the author. Proper credit must be attributed to the author when using or referencing this material.
 """
 
 import zipfile
@@ -24,11 +20,11 @@ from urllib.parse import urlparse, unquote
 import pathlib
 
 
-def download_zipped_csv_data(dataset_root_url, dataname, target_dir=None, local_root_path=None, verbose=False):
+def download_data(dataset_root_url, dataname, target_dir=None, local_root_path=None, verbose=False):  # noqa: E501
     """
     Download and extracts a zip file containing CSV data from a specified URL.
 
-    This function downloads a zip file from the provided dataset URL, extracts its contents, and saves them 
+    This function downloads a zip file from the provided dataset URL, extracts its contents, and saves them  # noqa: E501
     in the specified directory. If no target directory is provided, it uses the dataset name to create one.
     The function can also provide verbose output detailing each step.
 
@@ -56,13 +52,11 @@ def download_zipped_csv_data(dataset_root_url, dataname, target_dir=None, local_
     zipfile.BadZipFile
         If the downloaded zip file is corrupted, the function will raise this error and terminate.
     Exception
-        If any other error occurs during the process, it will be captured, and a message will be printed if 
-        verbose is enabled.
+        If any other error occurs during the process, it will be captured, and a message will be printed if verbose is enabled.
 
     Examples
     --------
     Download and extract a dataset with verbose output:
-    
     >>> download_zipped_csv_data('https://example.com/data', 'dataset.zip', target_dir='data', verbose=True)
     """
     if not local_root_path:
